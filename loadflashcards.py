6165b315fbffbc804ff15c54cc8ef696eba00b56
pymaster9 <adi31tya@icloud.com>
2024-07-31 03:14:28 -0400

    Making my flasbcardsApp give definitions

--- loadflashcards.py
+++ loadflashcards.py
@@ -4,14 +4,34 @@ import pickle
 import random
 root = tkinter.Tk()
 outputvar = tkinter.StringVar(root)
+userinput = tkinter.StringVar(root)
 #REPLACE
 setname = input("What set do you want?: ")
 with open(setname, "rb") as txt:
     set = pickle.load(txt)
+    print(set)
 
-set = random.shuffle(set)
+random.shuffle(set)
 idx = 0
-def showterm():
-    outputvar = "What is meant by" + set[idx].term
+outputvar.set("What is meant by " + set[idx].term)
+def check():
+    global idx
+    print(idx)
+    try:
+        if idx == -2:
+            root.quit()
+            return
+        if userinput.get() == set[idx].definition:
+            idx += 1
+            outputvar.set("Correct! Now, what is meant by "+ set[idx].term)
+            main.playSound("restartSession.mp3")
+        else:
+            outputvar.set("Incorrect. " + set[idx].term + " actually means " + set[idx].definition + ". Now, what is meant by " + set[idx + 1].term)
+            idx += 1
+    except IndexError:
+        idx = -2
+        outputvar.set('Set done! Now, just press the "Check" button to end the session.')
 tkinter.Label(root, textvariable=outputvar).pack()
+tkinter.Entry(root, textvariable=userinput).pack()
+tkinter.Button(root, text="Check", command=check).pack()
 root.mainloop()
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+import pygame
+
+def playSound(sound):
+    pygame.mixer.music.load(os.getcwd() + "/" + sound)
+    pygame.mixer.music.play(loops=0)
+
 class Flashcard:
     def __init__(self, term, definition):
         self.term = term
